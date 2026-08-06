c05918eace59254c6f20de09a84a7a849f72eee5
andrebarth <a.barth@escola.pr.gov.br>
2026-08-06 08:37:44 -0300

    2

--- script.js
+++ script.js
@@ -43,3 +43,11 @@ const perguntas = [
     ],
   },
 ];
+
+let atual = 0;
+let perguntaAtual;
+ function mostraPergunta() {
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+}
+mostraPergunta();
