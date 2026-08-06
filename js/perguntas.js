e408bbedf6008fd9429c90889755652c1ef294b1
andrebarth <a.barth@escola.pr.gov.br>
2026-08-06 13:13:18 -0300

    22

--- js/perguntas.js
+++ js/perguntas.js
@@ -3,14 +3,14 @@ export const perguntas = [
         enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
         alternativas: [
             {
-                texto: "Isso é assustador!",
+                texto: "Isso é assustadouro!",
                 afirmacao: [
                     "No início ficou com medo do que essa tecnologia pode fazer.",
                     "Achou assustador pensar na velocidade na qual a tecnologia está avançando."
                 ]
             },
             {
-                texto: "Isso é maravilhoso!",
+                texto: "Isso é maravilhosissimo!",
                 afirmacao: [
                     "Quis saber como usar IA no seu dia a dia.",
                     "Pensou que essa nova tecnologia pode ajudar em várias tarefas do seu dia a dia. "
